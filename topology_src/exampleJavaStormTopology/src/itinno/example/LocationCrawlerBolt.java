dda686e87f325d72e80c224795d1f467d12f2383
nico1510 <Nicolas-Beck@gmx.de>
2015-02-11 13:39:43 +0100

    adapted to also support non-dbpedia uris

--- topology_src/exampleJavaStormTopology/src/itinno/example/LocationCrawlerBolt.java
+++ topology_src/exampleJavaStormTopology/src/itinno/example/LocationCrawlerBolt.java
@@ -11,9 +11,6 @@ import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Values;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.hp.hpl.jena.query.ParameterizedSparqlString;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
@@ -21,6 +18,7 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.RDFNode;
@@ -28,6 +26,7 @@ import com.hp.hpl.jena.rdf.model.SimpleSelector;
 import com.hp.hpl.jena.rdf.model.impl.ResourceImpl;
 import com.hp.hpl.jena.util.FileManager;
 import com.hp.hpl.jena.vocabulary.OWL;
+import com.hp.hpl.jena.vocabulary.RDFS;
 import itinno.common.StormLoggingHelper;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -241,35 +240,41 @@ public class LocationCrawlerBolt extends BaseRichBolt {
         }
     }
 
-    private static Map<String, String> dereferenceLocation(String key) {
-        HashMap<String, String> resultMap = new HashMap<>();
+
+    private static Map<String, Literal> dereferenceLocation(String locationUri) {
+        HashMap<String, Literal> resultMap = new HashMap<>();
+        Model locationTriples = ModelFactory.createDefaultModel().read(locationUri);
+        System.out.println("read in");
+        
         ParameterizedSparqlString queryString = new ParameterizedSparqlString(
                 "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>"
-                + "SELECT ?lat ?long WHERE {"
+                + "SELECT ?lat ?long ?label WHERE {"
                 + "  ?s geo:lat ?lat ."
-                + "  ?s geo:long ?long .}");
-        queryString.setIri("?s", key);
-
+                + "  ?s geo:long ?long ."
+                + "  OPTIONAL { ?s ?rdfs_label ?label . }"
+                        + " }");
+        queryString.setIri("?s", locationUri);
+        queryString.setIri("?rdfs_label", RDFS.label.getURI());
+        
         Query query = QueryFactory.create(queryString.asQuery());
         ResultSet results;
-        try (QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query)) {
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, locationTriples)) {
             results = qexec.execSelect();
             if (results.hasNext()) {
                 QuerySolution result = results.next();
-                resultMap.put("location", key);
-                resultMap.put("lat", result.get("?lat").toString());
-                resultMap.put("long", result.get("?long").toString());
+                resultMap.put("location", (Literal)locationTriples.createLiteral(locationUri));
+                resultMap.put("lat", (Literal)result.get("?lat"));
+                resultMap.put("long", (Literal)result.get("?long"));
+                resultMap.put("label", (Literal)result.get("?label"));
                 return resultMap;
             } else {
                 return null;
             }
         }
-
     }
 
     /**
-     * Execute received Tuple. In this case we will simply print received
-     * output.
+     * searches for locations in the message and computes related locations
      *
      * @param input standard Storm tuple input object (passed within Storm
      * topology itself, not be a user)
@@ -280,8 +285,8 @@ public class LocationCrawlerBolt extends BaseRichBolt {
         Map<Object, Object> inputMap = (HashMap<Object, Object>) input.getValue(0);
         // Get JSON object from the HashMap from the Collections.singletonList
         JSONObject message = (JSONObject) Collections.singletonList(inputMap.get("message")).get(0);
-        ArrayList<Map<String, String>> relatedLocations = new ArrayList<>();
-        
+        ArrayList<Map<String, Literal>> relatedLocations = new ArrayList<>();
+
         // Print received message
         this.logger.info("Received message: " + message.toJSONString());
         if (message.containsKey("itinno:loc_set")) {
@@ -289,14 +294,14 @@ public class LocationCrawlerBolt extends BaseRichBolt {
 
             for (int i = 0; i < locationSet.size(); i++) {
                 JSONArray containerArray = (JSONArray) locationSet.get(i);
-                String linkedGeoDataUri = (String)((JSONArray) containerArray.get(10)).get(0);
+                String linkedGeoDataUri = (String) ((JSONArray) containerArray.get(10)).get(0);
 
                 String dbPediaUri = mapToDBPedia(linkedGeoDataUri);
                 if (dbPediaUri != null) {
                     Map<String, ArrayList<String>> possibleLocations = lookUpDBPediaUri(dbPediaUri);
                     for (Entry<String, ArrayList<String>> e : possibleLocations.entrySet()) {
                         if (checkCandidateBasedOnProperties(e.getValue())) {
-                            Map<String, String> locationWithCoordinates = dereferenceLocation(e.getKey());
+                            Map<String, Literal> locationWithCoordinates = dereferenceLocation(e.getKey());
                             if (locationWithCoordinates != null) {
                                 relatedLocations.add(locationWithCoordinates);
                             }
@@ -313,8 +318,8 @@ public class LocationCrawlerBolt extends BaseRichBolt {
     }
 
     /**
-     * Declare output field name (in this case it is not used because printer
-     * bolt will not emit any messages to other bolts)
+     * Declare output field name (in this case simple a string value that is
+     * defined in the constructor call)
      *
      * @param declarer standard Storm output fields declarer
      */
