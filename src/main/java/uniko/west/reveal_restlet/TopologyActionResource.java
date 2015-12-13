a756f0bbc0d4fb01e28a8f25382c0f7d8ebfe81c
mkrnr <info@mkoerner.de>
2015-12-13 23:58:10 +0100

    Modify restlet calls to include the exchange name in the requests
    The calls now require the (less confusing) paramenter "exchange" instead
    of "channel"

--- src/main/java/uniko/west/reveal_restlet/TopologyActionResource.java
+++ src/main/java/uniko/west/reveal_restlet/TopologyActionResource.java
@@ -5,20 +5,23 @@
  */
 package uniko.west.reveal_restlet;
 
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.thrift7.TException;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import backtype.storm.generated.KillOptions;
 import backtype.storm.generated.Nimbus;
 import backtype.storm.generated.Nimbus.Client;
 import backtype.storm.generated.NotAliveException;
 import backtype.storm.utils.NimbusClient;
 import backtype.storm.utils.Utils;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.apache.thrift7.TException;
-import org.restlet.resource.Get;
-import org.restlet.resource.ServerResource;
 
 /**
  *
@@ -26,103 +29,123 @@ import org.restlet.resource.ServerResource;
  */
 public class TopologyActionResource extends ServerResource {
 
-    String topologyId;
-    String action;
-    String channelId;
+	String topologyFolderName;
+	String topologyName;
+	String action;
+	String exchangeName;
 
-    String status;
-    boolean success;
+	String status;
+	boolean success;
 
-    @Override
-    public void doInit() {
-        this.topologyId = getAttribute("topology");
-        this.action = getAttribute("action");
-        this.channelId = getQueryValue("channel");
-        Map conf = Utils.readStormConfig();
-        Nimbus.Client client;
-        try {
-            client = NimbusClient.getConfiguredClient(conf).getClient();
-        } catch (Exception ex) {
-            success = false;
-            status = "Failed to connect to Nimbus!";
-            Logger.getLogger(TopologyActionResource.class.getName()).log(Level.SEVERE, null, ex);
-            return;
-        }
-        switch (action) {
-            case "stop":
-                stopTopology(client);
-                break;
-            case "start":
-                startTopology(client);
-                break;
-            case "kill":
-                killTopology(client);
-                break;
-            case "deploy":
-                deployTopology();
-                break;
-        }
-    }
+	@Override
+	public void doInit() {
+		this.topologyFolderName = this.getAttribute("topology");
+		this.action = this.getAttribute("action");
+		this.exchangeName = this.getQueryValue("exchange");
+		this.topologyName = this.exchangeName + "_" + this.topologyFolderName;
+		Map conf = Utils.readStormConfig();
+		Nimbus.Client client;
+		try {
+			client = NimbusClient.getConfiguredClient(conf).getClient();
+		} catch (Exception ex) {
+			this.success = false;
+			this.status = "Failed to connect to Nimbus!";
+			Logger.getLogger(TopologyActionResource.class.getName()).log(Level.SEVERE, null, ex);
+			return;
+		}
+		switch (this.action) {
+		case "stop":
+			this.stopTopology(client);
+			break;
+		case "start":
+			this.startTopology(client);
+			break;
+		case "kill":
+			this.killTopology(client);
+			break;
+		case "deploy":
+			this.deployTopology();
+			break;
+		}
+	}
 
-    @Get(value = "json")
-    @Override
-    public String toString() {
-        ObjectNode responseObject = new ObjectNode(JsonNodeFactory.instance);
-        responseObject.put("success", success);
-        responseObject.put("status", status);
+	@Get(value = "json")
+	@Override
+	public String toString() {
+		ObjectNode responseObject = new ObjectNode(JsonNodeFactory.instance);
+		responseObject.put("success", this.success);
+		responseObject.put("status", this.status);
 
-        return responseObject.toString();
-    }
+		return responseObject.toString();
+	}
 
-    private void stopTopology(Client client) {
-        try {
-            client.deactivate(topologyId);
-            success = true;
-            status = "Topology " + topologyId + " is now inactive";
-        } catch (NotAliveException | TException ex) {
-            success = false;
-            Logger.getLogger(TopologyActionResource.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	private void stopTopology(Client client) {
+		try {
+			if (this.exchangeName != null) {
+				client.deactivate(this.topologyName);
+				this.success = true;
+				this.status = "Topology " + this.topologyName + " is now inactive";
+			} else {
+				this.success = false;
+				this.status = "The exchange parameter needs to be set with: .../stop?exchange=<exchange-name>";
+			}
+		} catch (NotAliveException | TException ex) {
+			this.success = false;
+			Logger.getLogger(TopologyActionResource.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    private void startTopology(Client client) {
-        try {
-            client.activate(topologyId);
-            success = true;
-            status = "Topology " + topologyId + " is now active";
-        } catch (NotAliveException | TException ex) {
-            success = false;
-            Logger.getLogger(TopologyActionResource.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	private void startTopology(Client client) {
+		try {
+			if (this.exchangeName != null) {
+				client.activate(this.topologyName);
+				this.success = true;
+				this.status = "Topology " + this.topologyName + " is now active";
+			} else {
+				this.success = false;
+				this.status = "The exchange parameter needs to be set with: .../start?exchange=<exchange-name>";
+			}
+		} catch (NotAliveException | TException ex) {
+			this.success = false;
+			Logger.getLogger(TopologyActionResource.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    private void killTopology(Client client) {
-        try {
-            KillOptions killOpts = new KillOptions();
-            killOpts.set_wait_secs(2);
-            client.killTopologyWithOpts(topologyId, killOpts);
-            success = true;
-            status = "Topology " + topologyId + " was killed";
-        } catch (NotAliveException | TException ex) {
-            success = false;
-            Logger.getLogger(TopologyActionResource.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	private void killTopology(Client client) {
+		try {
+			if (this.exchangeName != null) {
+				KillOptions killOpts = new KillOptions();
+				killOpts.set_wait_secs(2);
+				client.killTopologyWithOpts(this.topologyName, killOpts);
+				this.success = true;
+				this.status = "Topology " + this.topologyName + " was killed";
+			} else {
+				this.success = false;
+				this.status = "The exchange parameter needs to be set with: .../kill?exchange=<exchange-name>";
+			}
+		} catch (NotAliveException | TException ex) {
+			this.success = false;
+			Logger.getLogger(TopologyActionResource.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    private void deployTopology() {
-        try {
-            if (channelId != null) {
-                MavenRunner.deployTopology(topologyId, channelId);
-                success = true;
-                status = "Topology " + topologyId + " was deployed and is listening on channel " + channelId;
-            } else {
-                success = false;
-                status = "channel must not be null";
-            }
-        } catch (Exception ex) {
-            success = false;
-            status = "topology " + topologyId + " is already deployed to the cluster or was not found in directory " + StaticInformation.topologySrcDir;
-            Logger.getLogger(TopologyActionResource.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	private void deployTopology() {
+		try {
+			if (this.exchangeName != null) {
+				MavenRunner.deployTopology(this.topologyFolderName, this.exchangeName);
+				this.success = true;
+				this.status = "Topology " + this.topologyName + " was deployed and is listening to exchange "
+						+ this.exchangeName;
+			} else {
+				this.success = false;
+				this.status = "The exchange parameter needs to be set with: .../deploy?exchange=<exchange-name>";
+			}
+		} catch (Exception ex) {
+			this.success = false;
+			this.status = "topology " + this.topologyName
+					+ " is already deployed to the cluster or was not found in directory "
+					+ StaticInformation.topologySrcDir;
+			Logger.getLogger(TopologyActionResource.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
